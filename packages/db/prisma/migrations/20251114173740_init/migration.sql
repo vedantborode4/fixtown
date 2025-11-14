db41efe9d9fff70453727faca3299614dd642a9d
Vedant <borodevedant4@gmail.com>
2025-11-14 23:11:55 +0530

    feat(db): add Prisma Postgres database with basic User model

---
+++ packages/db/prisma/migrations/20251114173740_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'PRO', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+    "avatarUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "public"."User"("phone");
